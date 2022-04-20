ca33dea50f57966c214a4e6808bd25a9103a3515
Zoran Stevovic <zstevovich@gmail.com>
2022-04-21 00:16:41 +0200

    Add Doctrine ORM to project Add Postgres container

--- src/Infrastructure/DataAccess/Repositories/DeveloperRepository.php
+++ src/Infrastructure/DataAccess/Repositories/DeveloperRepository.php
@@ -8,22 +8,29 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Exception;
+use RuntimeException;
 
 class DeveloperRepository extends EntityRepository implements DeveloperRepositoryInterface
 {
+    private string $entity = "Architecture\Domain\Entities\Developer";
+
     public function __construct(EntityManager $entityManager)
     {
-        $metadata = new ClassMetadata('Architecture\Domain\Entities\Developer');
+        $metadata = new ClassMetadata($this->entity);
         parent::__construct($entityManager, $metadata);
     }
 
-    /**
-     * @throws Exception
-     */
     public function create(Developer $developer): Developer
     {
-        $this->getEntityManager()->getUnitOfWork()->persist($developer);
-        $this->getEntityManager()->getUnitOfWork()->commit();
+        $this->getEntityManager()->beginTransaction();
+        try {
+            $this->getEntityManager()->persist($developer);
+            $this->getEntityManager()->flush();
+            $this->getEntityManager()->commit();
+        } catch (Exception $e) {
+            $this->getEntityManager()->rollback();
+            throw new RuntimeException($e->getMessage());
+        }
         return $developer;
     }
 }
